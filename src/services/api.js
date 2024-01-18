c9d33aece48db0713752dc4b261cd4b0a74a3353
Diego Donoso <diegodonoso1998@gmail.com>
2024-01-18 20:39:22 -0300

    cambio link

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "http://localhost:3000/";
+const API_URL = "https://exalted-crane-405616.rj.r.appspot.com/";
 const api = axios.create({
   baseURL: API_URL,
   headers: {
@@ -11,3 +11,6 @@ export default api;
 
 //produccion
 //https://exalted-crane-405616.rj.r.appspot.com/
+
+//local
+//http://localhost:3000/
